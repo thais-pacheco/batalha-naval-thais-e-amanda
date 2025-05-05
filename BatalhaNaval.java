8e22b917e14f3b0c8a5d92c196d8dcae2b9733f0
thais <thais-pacheco@educar.rs.gov.br>
2025-05-05 15:45:36 -0300

    Estrutura inicial e constantes

--- BatalhaNaval.java
+++ BatalhaNaval.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class BatalhaNaval {
+    static final int TAMANHO = 10;
+    static final char AGUA = '~';
+    static final char NAVIO = 'N';
+    static final char TIRO = 'X';
+    static final char ERRO = 'O';
+
+    static Scanner scanner = new Scanner(System.in);
+    static Random rand = new Random();
+
+    static String nome1, nome2;
+    static boolean contraComputador = false;
+
+    public static void main(String[] args) {
+        // Lógica principal será implementada nos próximos commits
+    }
 }
